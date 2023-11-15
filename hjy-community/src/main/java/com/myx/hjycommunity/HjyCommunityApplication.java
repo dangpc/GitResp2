89a3566d61817a985758968b5e341953d71c94a9
dangpc <HCLYQ0429@163.com>
2023-11-15 22:30:19 +0800

    合家云项目第一次提交

---
+++ hjy-community/src/main/java/com/myx/hjycommunity/HjyCommunityApplication.java
@@ -0,0 +1,17 @@
+package com.myx.hjycommunity;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.myx.hjycommunity.**.mapper")
+public class HjyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjyCommunityApplication.class, args);
+
+        System.out.println("合家云后台系统启动成功！");
+    }
+
+}
---
+++ hjy-community/src/test/java/com/myx/hjycommunity/HjyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myx.hjycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
